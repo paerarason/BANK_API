26d8ce9655b817c3073a062e83a28392ea2af106
your_github_username <paerarason1111@gmail.com>
2023-02-01 20:58:58 +0530

    readme.txt added

--- BANK_API/API/urls.py
+++ BANK_API/API/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path,include
 from . import views
 urlpatterns=[
-    path('ban/',views.branch),
+    path('branch/',views.branch),
     path('bank/',views.bank),
-    path('branch/<str:location->/',views.select,name="branch_code"),
+    path('branch/<str:location>/',views.select,name="branch_code"),
     path('bank/<str:pk>/',views.by_ifsc,name="ifsc_code")
 ]
